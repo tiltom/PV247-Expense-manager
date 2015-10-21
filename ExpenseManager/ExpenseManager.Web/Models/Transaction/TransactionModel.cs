1beda36e45513ac9150d5a86b865ba8cc078ea13
Ján Mošať <jmosat@gmail.com>
2015-10-21 02:44:58 +0200

    [#7] Should repeatable Transaction look like this?

--- ExpenseManager/ExpenseManager.Web/Models/Transaction/TransactionModel.cs
+++ ExpenseManager/ExpenseManager.Web/Models/Transaction/TransactionModel.cs
@@ -17,8 +17,7 @@ namespace ExpenseManager.Web.Models.Transaction
 
         [Required]
         [DataType(DataType.DateTime)]
-        //[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
 
         public string Description { get; set; }
@@ -28,12 +27,14 @@ namespace ExpenseManager.Web.Models.Transaction
 
         public virtual Budget Budget { get; set; }
 
+        [Display(Name = "Currency")]
         [Required]
         public string CurrencyId { get; set; }
 
         [Display(Name = "Currency")]
         public string CurrencySymbol { get; set; }
 
+        [Display(Name = "Category")]
         [Required]
         public string CategoryId { get; set; }
 
@@ -47,5 +48,16 @@ namespace ExpenseManager.Web.Models.Transaction
         public List<SelectListItem> Currencies { get; set; }
 
         public List<SelectListItem> Budgets { get; set; }
+
+        [Display(Name = "Repeat transaction?")]
+        public bool IsRepeatable { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
+        public TimeSpan Frequency { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Last Ocurence")]
+        public DateTime? LastOccurence { get; set; }
     }
 }
