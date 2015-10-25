74fb27cecd7f1b42e5464e3c0a9cb438f3020f40
Ján Mošať <jmosat@gmail.com>
2015-10-25 16:12:33 +0100

    [#7] Repeatable transaction implementation, new models for transactions

--- ExpenseManager/ExpenseManager.Web/Models/Transaction/NewTransactionModel.cs
+++ ExpenseManager/ExpenseManager.Web/Models/Transaction/NewTransactionModel.cs
@@ -5,18 +5,14 @@ using System.Web.Mvc;
 
 namespace ExpenseManager.Web.Models.Transaction
 {
-    public class TransactionModel
+    public class NewTransactionModel
     {
-        [Required]
-        public Guid Id { get; set; }
-
         [Required]
         public decimal Amount { get; set; }
 
-
         [Required]
-        [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date")]
         public DateTime Date { get; set; }
 
         public string Description { get; set; }
@@ -24,26 +20,19 @@ namespace ExpenseManager.Web.Models.Transaction
         [Required]
         public Guid WalletId { get; set; }
 
-        public virtual Entity.Budgets.Budget Budget { get; set; }
+        [Display(Name = "Budget")]
+        public string BudgetId { get; set; }
 
         [Display(Name = "Currency")]
         [Required]
         public string CurrencyId { get; set; }
 
-        [Display(Name = "Currency")]
-        public string CurrencySymbol { get; set; }
-
         [Display(Name = "Category")]
         [Required]
         public string CategoryId { get; set; }
 
-        [Display(Name = "Category")]
-        public string CategoryName { get; set; }
-
-        [Display(Name = "Select Category")]
         public List<SelectListItem> Categories { get; set; }
 
-        [Display(Name = "Select Currency")]
         public List<SelectListItem> Currencies { get; set; }
 
         public List<SelectListItem> Budgets { get; set; }
@@ -54,8 +43,8 @@ namespace ExpenseManager.Web.Models.Transaction
         [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan Frequency { get; set; }
 
-        [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Last Ocurence")]
         public DateTime? LastOccurence { get; set; }
     }
---
+++ ExpenseManager/ExpenseManager.Web/Models/Transaction/TransactionShowModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseManager.Web.Models.Transaction
+{
+    public class TransactionShowModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        [Display(Name = "Budget")]
+        public string BudgetName { get; set; }
+
+        [Display(Name = "Currency")]
+        public string CurrencySymbol { get; set; }
+
+        [Display(Name = "Category")]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "Repeatable transaction")]
+        public bool IsRepeatable { get; set; }
+    }
+}
