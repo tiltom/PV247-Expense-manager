f0ee60b545c49b9f2963f0f8fa41fded898a43f1
tiltom <tilnak.tomas@gmail.com>
2015-11-15 18:42:07 +0100

    #32 Fixed initialization of datepicker in Transactions/Edit

--- ExpenseManager/ExpenseManager.Web/Models/Transaction/NewTransactionModel.cs
+++ ExpenseManager/ExpenseManager.Web/Models/Transaction/NewTransactionModel.cs
@@ -22,6 +22,7 @@ namespace ExpenseManager.Web.Models.Transaction
         /// </summary>
         [Required]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Date")]
         public DateTime Date { get; set; }
 
