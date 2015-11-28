c6ece55098e80aba7f8bff77ce41af04356bdd56
Slavomír Krupa <slavomir.krupa@gmail.com>
2015-11-28 21:20:28 +0100

    added web safe colors for graphs

--- ExpenseManager/ExpenseManager.BusinessLogic/CommonServices/ColorGeneratorService.cs
+++ ExpenseManager/ExpenseManager.BusinessLogic/CommonServices/ColorGeneratorService.cs
@@ -12,14 +12,7 @@ namespace ExpenseManager.BusinessLogic.CommonServices
 
         private static readonly string[] ColourValues =
         {
-            "FF0000", "00FF00", "0000FF", "FFFF00", "FF00FF", "00FFFF", "000000",
-            "800000", "008000", "000080", "808000", "800080", "008080", "808080",
-            "C00000", "00C000", "0000C0", "C0C000", "C000C0", "00C0C0", "C0C0C0",
-            "400000", "004000", "000040", "404000", "400040", "004040", "404040",
-            "200000", "002000", "000020", "202000", "200020", "002020", "202020",
-            "600000", "006000", "000060", "606000", "600060", "006060", "606060",
-            "A00000", "00A000", "0000A0", "A0A000", "A000A0", "00A0A0", "A0A0A0",
-            "E00000", "00E000", "0000E0", "E0E000", "E000E0", "00E0E0", "E0E0E0"
+            "#727272", "#f1595f", "#79c36a", "#599ad3", "#f9a65a", "#9e66ab", "#cd7058", "#d77fb3"
         };
 
         private int _index;
@@ -31,9 +24,15 @@ namespace ExpenseManager.BusinessLogic.CommonServices
         /// <returns>code of color in #RRGGBB format</returns>
         public string GenerateColorForIndex(int index)
         {
+            var normalizedIndex = 1;
+            if (index != int.MinValue)
+            {
+                normalizedIndex = Math.Abs(index)%ColourValues.Length;
+            }
+
             // make number in range of array (currently used for color generation)
-            var normalizedIndex = Math.Abs(index)%ColourValues.Length;
-            return "#" + ColourValues[normalizedIndex];
+
+            return ColourValues[normalizedIndex];
         }
 
         /// <summary>
@@ -46,7 +45,7 @@ namespace ExpenseManager.BusinessLogic.CommonServices
             {
                 this._index = 0;
             }
-            return "#" + ColourValues[this._index++];
+            return ColourValues[this._index++];
         }
     }
 }
